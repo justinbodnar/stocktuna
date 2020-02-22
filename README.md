d34fc17cb947b840b3141a175a477dca12320b2c
Justin Bodnar <justin.bodnar@wilkes.edu>
2020-02-22 10:33:52 -0500

    updated readme with new proposed data architectures

--- README.md
+++ README.md
@@ -5,26 +5,68 @@ QUESTION: How wo we create a data set to train on historical trends?
 	- using monte carlo simulations of course!
 	- create random investments on random days in history
 	- calculate the resulting delta
-	- a datum = 1 investment
+	- 1 data point = 1 investment
 
 DATA SET level 0 - least amount of information
 
 	- we're asking the question:
-	- 'Considering the past n days of price changes, should I invest, and for how long?'
-	- datum contains:
+	- 'Considering the past n days of price changes, can this be a good investment?'
+	- data point contains:
 		- n days of closing price history
-		- time delta
-	- tag contains boolean
-		- delta < 0
+                        - double[2*n] of [ open_1, close_1, open_2, close_2, ...., open_n, close_n ]
+                        - where day_n is the day before date_bought
+	- tag contains:
+		- boolean: delta > 0
+	- ie. Invest $100 into Rite Aid on Christmas, sold for $105 2 days later would create the following data_point and tag
+		- data_point = [ open_1, close_1, open_2, close_2 ]
+		- tag        = [ true ]
 
 DATA SET level 1 - moderate amount of information
 
 	- we're taking a time based risk, like range trading
-	- datum contains:
-		- 
-# Stonks.py
+	- we're asking the question:
+	- 'Considering the past n days of price changes, should I invest for x days?'
+	- data point contains:
+                - time delta, in days the shares were held
+		- n days of of open/close data as:
+			- double[2*n] of [ open_1, close_1, open_2, close_2, ...., open_n, close_n ] 
+			- where day_n is the day before date_bought
+	- tag contains:
+		- boolean: delta > 0
+        - ie. Invest $100 into Rite Aid on Christmas, sold for a $5.00 PROFIT 2 days later would create the following data_point and tag
+                - data_point = [ 2, open_1, close_1, open_2, close_2 ]
+                - tag        = [ true ]
+
+DATA SET level 3 - all information available in yahoo finance api
+
+	- the most information available from the api is:
+		- n previous days' data including:
+			- Open
+	                - High
+	                - Low
+	                - Close
+	                - Adj Close
+	                - Volume
+	- we're asking the question:
+	- 'Considering all available information, should I invest for x days?'
+	- data point contains all lists flattened along the rows:
+		- [ days_held,
+		  open_1, open_2, ..., open_n,
+		  high_1, high_2, ..., high_n,
+		  low_1, low_2, ..., low_n,
+		  close_1 close_2, ..., close_n,
+		  adjc_1, adjc_2, ..., adj_n,
+		  vol_1, vol_2, ..., vol_n ]
+	- tag contains:
+		- boolean: delta > 0
+
+# Stonks.py functions defined
 
 def random_dates():
+
+	- returns two random dates; date_1 and date_2
+	- date_2 is (1-30) days after date_1
+	- format: YYYY-MM-DD
 
 def random_investment( stock, amount_invested ):
 
--- Stonks.py
+++ Stonks.py
@@ -5,42 +5,44 @@
 import matplotlib.pyplot as plt
 import yfinance as yf
 import random
+import time
+import signal
+
+# signal handler funct
+def signal_handler(sgnum, frame):
+	raise Exception("Timed out!")
 
 # random dates function
 # format: YYYY-MM-DD
 def random_dates():
 
+	# this is such an awful naive solution
+	# bruteforced rapid typed solution used to save dev time
+	# codersdebt++
+
+	# grab a random month
 	m = random.randrange(12)
+
+	# determine how many days are in the random month
 	if m == 0:
 		d = 32
 	elif m == 1:
 		d = 28
-	elif m == 2:
-		d = 31
-	elif m == 3:
-		d = 30
 	elif m == 4:
 		d = 29
-	elif m == 5:
-		d = 30
-	elif m == 6:
+	elif m == 6 or m == 7 or m == 11 or m == 2:
 		d = 31
-	elif m == 7:
-		d = 31
-	elif m == 8:
+	elif m == 3 or m == 5 or m == 8 or m == 9 or m == 10:
 		d = 30
-	elif m == 9:
-		d = 30
-	elif m == 10:
-		d = 30
-	elif m == 11 :
-		d = 31
 
+	# grab a random year
 	y = random.randrange(2010,2019)
 
 	# casting as strings
 	y = str(y)
 	m += 1
+
+	# padding months
 	if m > 9:
 		m = str(m)
 	else:
@@ -51,30 +53,56 @@ def random_dates():
 		d = str(d)
 	datestamp = y+"-"+m+"-"+d
 
+	# start on second date
+	# should be < 30 days per investment
+
 	# inc month
-	if int(m) == 12:
-		m = "01"
+	if int(m) == 12: # for christmas time investments i guess
+		m = 1
+		y = str( int(y)+1 )
 	else:
-		m = str( int(m)+1 )
+		m = int(m)+1
+
+	# padding month
+	if m < 10:
+		m = "0"+str(m)
+	else:
+		m = str(m)
 
 	# random day
-	d = str(random.randrange(28))
+	d = random.randrange(27)+1
 
+	# padding day
+	if d < 9:
+		d = "0"+str(d)
+	else:
+		d = str(d)
+
+	# concat second datestamp string
 	datestamp2 = y+"-"+m+"-"+d
 
-	# return datestamp
+	# return datestamps
 	return datestamp, datestamp2
 
 # random_investment function
-# takes a stock, date_bought, amount_invested, date_sold
+# takes a stock
 # returns float of delta
 # uses yahoo finance api
 # assumes bought at open price
 # and sold at close price
-def random_investment( stock, amount_invested ):
+def random_investment( stock ):
+
+	# amount_invested is arbitrary
+	amount_invested = 100.0
+
+	# start timer
+	signal.signal(signal.SIGALRM, signal_handler)
+	signal.alarm(1) # in seconds
 
 	# get random dates
 	date_bought, date_sold = random_dates()
+
+	print( date_bought, date_sold )
 
 	# get the data set from yahoo finace api
 	data = yf.download( stock, date_bought, date_sold )
@@ -95,8 +123,8 @@ def random_investment( stock, amount_invested ):
 	delta = sold - amount_invested
 
 	# output
-	print( "Bought " + str(amount_invested) + " on " + date_bought )
-	print( "Sold on " + date_sold + " for a total of " + str(round(sold,2)) )
+	print( "Bought on: " + date_bought + " for a price of: " + str(amount_invested) )
+	print( "Sold on:   " + date_sold + " for a total of:   " + str(round(sold,2)) )
 
 	# return delta
 	return delta
@@ -111,7 +139,7 @@ while i < 10:
 
 	try:
 		date1, date2 = random_dates()
-		delta = random_investment( stock, 100.0 )
+		delta = random_investment( stock )
 		print( "Delta: " + str(round(delta,3)) )
 
 	except Exception as e:
@@ -122,5 +150,4 @@ while i < 10:
 #delta = investment( "RAD", "2018-02-01", "2018-02-28", 1000 )
 #print( "Delta: " + str(round(delta,3)) )
 
-randomdate = random_date()
-print(randomdate)
+randomdate = random_dates()
